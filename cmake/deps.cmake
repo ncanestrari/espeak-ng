afb288e4dc7d828bfc62f9191b866f7ccef8e6eb
ncanestrari <niccolo.canestrari@alten.com>
2024-09-03 11:57:55 +0200

    Avoid Fetching dependencies if they are not used

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,9 +1,18 @@
+if(USE_LIBSONIC)
 find_library(SONIC_LIB sonic)
 find_path(SONIC_INC "sonic.h")
+endif(USE_LIBSONIC)
+
+if(USE_LIBPCAUDIO)
 find_library(PCAUDIO_LIB pcaudio)
 find_path(PCAUDIO_INC "pcaudiolib/audio.h")
+endif(USE_LIBPCAUDIO)
+
 find_library(PTHREAD_LIB pthread)
+
+if(USE_MBROLA)
 find_program(MBROLA_BIN mbrola)
+endif(USE_MBROLA)
 
 include(FetchContent)
 
@@ -15,7 +24,7 @@ if (MBROLA_BIN)
 endif(MBROLA_BIN)
 if (SONIC_LIB AND SONIC_INC)
   set(HAVE_LIBSONIC ON)
-else()
+elseif(USE_LIBSONIC)
   FetchContent_Declare(sonic-git
     GIT_REPOSITORY https://github.com/waywardgeek/sonic.git
     GIT_TAG fbf75c3d6d846bad3bb3d456cbc5d07d9fd8c104
@@ -27,6 +36,8 @@ else()
   set(HAVE_LIBSONIC ON)
   set(SONIC_LIB sonic)
   set(SONIC_INC ${sonic-git_SOURCE_DIR})
+else()
+  set(HAVE_LIBSONIC OFF)
 endif()
 if (PCAUDIO_LIB AND PCAUDIO_INC)
   set(HAVE_LIBPCAUDIO ON)
